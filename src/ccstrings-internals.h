3799bb377a52e9bca462f0059d4b2bd8e4a4ec83
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-02-16 08:26:18 +0100

    fixed license notices

--- src/ccstrings-internals.h
+++ src/ccstrings-internals.h
@@ -8,25 +8,22 @@
 	This  header  file is  for  internal  definitions.  It  must  be
 	included by all the source files in this package.
 
-  Copyright (C) 2017 Marco Maggi <marco.maggi-ipsu@poste.it>
+  Copyright (C) 2017, 2019 Marco Maggi <marco.maggi-ipsu@poste.it>
 
-  This program is  free software: you can redistribute  it and/or modify
-  it  under the  terms  of  the GNU  Lesser  General  Public License  as
-  published by  the Free  Software Foundation, either  version 3  of the
-  License, or (at your option) any later version.
+  This program is free  software: you can redistribute it and/or  modify it under the
+  terms of the  GNU Lesser General Public  License as published by  the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later version.
 
-  This program  is distributed in the  hope that it will  be useful, but
-  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
-  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
-  General Public License for more details.
+  This program  is distributed in the  hope that it  will be useful, but  WITHOUT ANY
+  WARRANTY; without  even the implied  warranty of  MERCHANTABILITY or FITNESS  FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
-  You  should have  received a  copy of  the GNU  Lesser General  Public
-  License     along    with     this    program.      If    not,     see
-  <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General Public License along with
+  this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CCS_INTERNALS_H
-#define CCS_INTERNALS_H 1
+#ifndef CCSTRINGS_INTERNALS_H
+#define CCSTRINGS_INTERNALS_H 1
 
 
 /** --------------------------------------------------------------------
@@ -69,6 +66,6 @@
 } // extern "C"
 #endif
 
-#endif /* CCS_INTERNALS_H */
+#endif /* CCSTRINGS_INTERNALS_H */
 
 /* end of file */
